1e7a0eeaf73f2de199431f12b7a9b12d8c2cac78
kanic1111 <daniel890723@gmail.com>
2020-05-06 20:55:45 +0800

    add code

--- raspi/mqtt_datatransfer.py
+++ raspi/mqtt_datatransfer.py
@@ -1,8 +1,35 @@
-import paho.mqtt.publish as publish
 import serial
+import time
+import paho.mqtt.publish as publish
+ 
+serial = serial.Serial('/dev/ttyUSB0',9600 , timeout = 2)
+def readandpub(serial):
+    print('read')
+    response = serial.readline()
+    if response != '':
+        publish.single("Sensor/DHT22", payload=response,
+                        qos=0, hostname="127.0.0.1", port=1883)
+        status = 1
+    else:
+        status = reconnect(serial)
+    return status
+def reconnect(serial):
+    print('re')
+    try:
+        serial.close()
+        serial.open()
+        status = 1
+        return status
+    except:
+        status = 0
+        return status
 
-serial = serial.Serial('COM13',9600 , timeout = 2)
-
-response = serial.readline()
-publish.single("Sensor/DHT22", payload=response,
-                 qos=0, hostname="192.168.43.59", port=1883)
+while True:
+    try:
+        status = readandpub(serial)
+        time.sleep(1)
+    except:
+        status = 0
+        while(status == 0):
+            status = reconnect(serial)
+            print("Arduino is reconnect")
--- raspi/mqtt_sub.py
+++ raspi/mqtt_sub.py
@@ -1,35 +1,13 @@
-import serial
-import time
-import paho.mqtt.publish as publish
- 
-serial = serial.Serial('/dev/ttyUSB0',9600 , timeout = 2)
-def readandpub(serial):
-    print('read')
-    response = serial.readline()
-    if response != '':
-        publish.single("Sensor/DHT22", payload=response,
-                        qos=0, hostname="127.0.0.1", port=1883)
-        status = 1
-    else:
-        status = reconnect(serial)
-    return status
-def reconnect(serial):
-    print('re')
-    try:
-        serial.close()
-        serial.open()
-        status = 1
-        return status
-    except:
-        status = 0
-        return status
+import paho.mqtt.client as mqtt
+import json
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("Sensor/DHT22")
+def on_message(client, userdata, msg):
+    data = json.loads(str(msg.payload.decode('ascii')))
+    print(data)
 
-while True:
-    try:
-        status = readandpub(serial)
-        time.sleep(1)
-    except:
-        status = 0
-        while(status == 0):
-            status = reconnect(serial)
-            print("Arduino is reconnect")
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("127.0.0.1", 1883)
+client.loop_forever()
